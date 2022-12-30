b6cb57b1f5cc2281b560a161b354edb4b5735418
takumab <takumab@gmail.com>
2022-12-30 17:55:47 +0000

    Refactor variable and parameter names

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,26 +1,25 @@
-export const stringCalculator = (str: string) => {
+export const stringCalculator = (stringOfNumbers: string) => {
   const checkForNegativeNumbersAndSeparators =
     /-(?=[A-Za-z-]|\d+-\d)|(\n|,|;|\/)/g;
 
-  if (hasNegativeNumbers(str, checkForNegativeNumbersAndSeparators))
+  if (hasNegativeNumbers(stringOfNumbers, checkForNegativeNumbersAndSeparators))
     throw new Error("negatives not allowed");
 
-  const strings = str.split(",");
-  for (let i = 0; i < strings.length; i++) {
-    if (isGreaterThan1000(strings)) return Number(strings[i + 1]);
+  const stringyfiedNumbers = stringOfNumbers.split(",");
+  for (let i = 0; i < stringyfiedNumbers.length; i++) {
+    if (isGreaterThan1000(stringyfiedNumbers))
+      return Number(stringyfiedNumbers[i + 1]);
   }
 
   const add = (total: number, num: string) => total + Number(num);
-  return str
+  return stringOfNumbers
     .replace(checkForNegativeNumbersAndSeparators, "")
     .split("")
     .reduce(add, 0);
 };
 
-const hasNegativeNumbers = (str: string, regEx: any) => {
-  return str.split(regEx).some((element) => Number(element) < 0);
-};
+const hasNegativeNumbers = (stringOfNumbers: string, regEx: any) =>
+  stringOfNumbers.split(regEx).some((num) => Number(num) < 0);
 
-const isGreaterThan1000 = (arr: string[]) => {
-  return arr.some((num) => Number(num) > 1000);
-};
+const isGreaterThan1000 = (stringyfiedNumbers: string[]) =>
+  stringyfiedNumbers.some((num) => Number(num) > 1000);
