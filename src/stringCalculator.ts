8b2789bfed62539a1c7fc1f6f00e1b7130a93bf2
takumab <takumab@gmail.com>
2023-01-30 18:54:36 +0000

    Add toNumber function to convert string to number

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -5,21 +5,24 @@ export const stringCalculator = (stringOfNumbers: string) => {
   if (hasNegativeNumbers(stringOfNumbers, checkForNegativeNumbersAndSeparators))
     throw new Error("negatives not allowed");
 
-  const stringyfiedNumbers = stringOfNumbers.split(",");
-  for (let i = 0; i < stringyfiedNumbers.length; i++) {
-    if (isGreaterThan1000(stringyfiedNumbers[i]))
-      return Number(stringyfiedNumbers[i + 1]);
+  const arrayOfNumbers = stringOfNumbers.split(",");
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if (isGreaterThan1000(arrayOfNumbers[i]))
+      return Number(arrayOfNumbers[i + 1]);
   }
 
-  const add = (total: number, num: string) => total + Number(num);
+  const add = (total: number, num: number) => total + num;
+  const toNumber = (num: string) => Number(num);
   return stringOfNumbers
     .replace(checkForNegativeNumbersAndSeparators, "")
     .split("")
+    .map(toNumber)
     .reduce(add, 0);
 };
 
 const hasNegativeNumbers = (stringOfNumbers: string, regEx: any) =>
   stringOfNumbers.split(regEx).some((num) => Number(num) < 0);
 
-const isGreaterThan1000 = (stringyfiedNumber: string) =>
-  Number(stringyfiedNumber) > 1000;
+const isGreaterThan1000 = (num: string) => {
+  return Number(num) > 1000;
+};
